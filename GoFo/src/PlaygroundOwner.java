f0768c4585920dbdf82060088a45549e0f626f29
NorhanAbdelkader <84976516+NorhanAbdelkader@users.noreply.github.com>
2021-06-12 22:29:35 +0200

    Update PlaygroundOwner.java

--- GoFo/src/PlaygroundOwner.java
+++ GoFo/src/PlaygroundOwner.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class PlaygroundOwner extends User{
+public class PlaygroundOwner {
   private int hours;
   private double cost;
   private String address;
   private Account account;
   private Playground playground;
-
-  static public int id = -1;
+  public  ArrayList<String> slots = new ArrayList<String>();
+  public static int id = -1;
+  private int id2 = 0;
 
   public PlaygroundOwner() {
     account = new Account();
@@ -16,15 +17,14 @@ public class PlaygroundOwner extends User{
     cost = 0;
     address = "";
     playground = new Playground();
-    id++;
-    /*if (id == Account.idPlaygroundOwner){
+    PlaygroundOwner.id++;
+    id2 = PlaygroundOwner.id;
+    if (id == Account.idPlaygroundOwner) {
       account = account.getAccountByID(id);
-      System.out.println(account.toString());
-    }*/
+    }
   }
 
   public void createPlayground() {
-    ArrayList<String> slots = new ArrayList<String>();
     Scanner in = new Scanner(System.in);
 
     System.out.print("Enter Playground address: ");
@@ -36,25 +36,22 @@ public class PlaygroundOwner extends User{
     System.out.print("Enter slot duration: ");
     int slotDuration = in.nextInt();
 
-    System.out.print("Enter number of hours: "); 
+    System.out.print("Enter number of hours: ");
     this.hours = in.nextInt();
 
-    System.out.println("Enter with format HH:HH "); 
+    System.out.println("Enter with format HH:HH ");
 
-    for(int i = 0; i < (hours/slotDuration); i++){
+    for (int i = 0; i < (hours / slotDuration); i++) {
       slots.add(in.next());
     }
     this.playground.setUpPlayground(slots, this.cost, this.address, this);
   }
 
-  public boolean viewRequests() {
-    return true;
-  }
-
   public void viewBooks() {
-
+    playground.ownerBooks(getID());
   }
-  public void viewCreatedPlayground(){
+
+  public void viewCreatedPlayground() {
     this.playground.printArrayList();
   }
 
@@ -62,13 +59,16 @@ public class PlaygroundOwner extends User{
 
     System.out.println("Choose what you want to do: ");
     System.out.println("1- Create a playground.");
-    System.out.println("2- View Requests.");
-    System.out.println("3- View Books.");
-
+    System.out.println("2- View booked playgrounds.");
+    System.out.println("3- Exit.");
   }
 
-  public Account getAccount(){
-    if (id == Account.idPlaygroundOwner){
+  public int getID() {
+    return id2;
+  }
+
+  public Account getAccount() {
+    if (id == Account.idPlaygroundOwner) {
       account = account.getAccountByID(id);
       return this.account;
     }
@@ -76,10 +76,8 @@ public class PlaygroundOwner extends User{
   }
 
   public String toString() {
-    return "PlaygroundOwner informaion: " + "\n" + "Playground owner ID: " + id + "\n";
+    return "PlaygroundOwner informaion: " + "\n" + "Playground owner ID: " + (getID() + 1) + "\n";
   }
 
-
 }
-
 

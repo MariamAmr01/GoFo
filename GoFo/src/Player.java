e1d16d64c581a85b2b7102c1d2cc07a7e172c9a9
NorhanAbdelkader <84976516+NorhanAbdelkader@users.noreply.github.com>
2021-06-12 22:26:59 +0200

    Update Player.java

--- GoFo/src/Player.java
+++ GoFo/src/Player.java
@@ -1,34 +1,17 @@
-package hw4;
-
 import java.util.*;
-/**
- * This Class represents a client account with balance and account number
- * 
- * @author Norhan Abdelkader
- * @author Mariam Amr
- * @author Nermeen Mohamed
- * @since 2021-06-10
- * 
- */
+
 public class Player {
 
 	private Account account;
 	private Playground playground;
 	private PlaygroundOwner owner;
 	private int choice;
-
-    /**
-     *
-     */
-    static public int id2 = -1;
+	static public int id2 = -1;
 	private int id3 = 0;
 	private ArrayList<Playground> playerBooked = new ArrayList<Playground>();
 	private ArrayList<String> playerSlot = new ArrayList<String>();
-	  
-    /**
-     *
-     */
-    public Player() {
+
+	public Player() {
 		account = new Account();
 		Player.id2++;
 		id3 = Player.id2;
@@ -37,77 +20,56 @@ public class Player {
 		}
 	}
 
-    /**
-     *
-     * @param i
-     */
-    public void bookPlayground(int i) {
+	public void bookPlayground(int i) {
 		playground = new Playground();
-		playground=Playground.availablePlaygrounds.get(i);
+		playground = Playground.availablePlaygrounds.get(i);
 		if (playground.availableHours.size() != 0) {
-			
+
 			setSuitableSlots(i);
-	        playerSlot.add(playground.availableHours.get(getSlots()));
+			playerSlot.add(playground.availableHours.get(getSlots()));
 			playground.availableHours.remove(getSlots());
 			this.playground.bookPlayground(i, this);
 
 			account.transferMoney(playground.getCost(Playground.bookedPlaygrounds.size() - 1),
-			playground.getOwner(i).getAccount());
+					playground.getOwner(i).getAccount());
 			playerBooked.add(Playground.bookedPlaygrounds.get(Playground.bookedPlaygrounds.size() - 1));
- 
+
+		} else {
+			System.out.println("No slots Available");
 		}
-		else {System.out.println("No slots Available");} 
 	}
 
-    /**
-     *
-     * @param i
-     */
-    public void setSuitableSlots(int i) {
+	public void setSuitableSlots(int i) {
 		Scanner in = new Scanner(System.in);
 		printSlots(i);
 		System.out.println("Choose wanted slot");
 		choice = in.nextInt();
 	}
 
-    /**
-     *
-     * @return
-     */
-    public int getSlots() {
+	public int getSlots() {
 		return (choice - 1);
 	}
 
-    /**
-     *
-     */
-    public void viewBooking() {
+	public void viewBooking() {
 		System.out.println("Your booked playgrounds: ");
 		for (int i = 0; i < playerBooked.size(); i++) {
-		      System.out.println((i + 1) + "- " + playerBooked.get(i));
-		    }
+			System.out.println((i + 1) + "- " + playerBooked.get(i));
+		}
 	}
 
-    /**
-     *
-     */
-    public void cancelBooking() {
+	public void cancelBooking() {
 		System.out.println("Choose playground to cancel: ");
 		viewBooking();
 		Scanner in = new Scanner(System.in);
-		int choice=in.nextInt();
+		int choice = in.nextInt();
 		String x = playerSlot.get(0);
 		playerSlot.remove(0);
-		playerBooked.remove(choice-1);
+		playerBooked.remove(choice - 1);
 		viewBooking();
-		playground.cancelBooking2(choice-1,x);
+		playground.cancelBooking2(choice - 1, x);
 	}
 
-    /**
-     *
-     * @return
-     */
-    public Account getAccount() {
+	public Account getAccount() {
 		if (id2 == Account.idPlayer) {
 			account = account.getAccountByID(id2);
 			return this.account;
@@ -115,22 +77,14 @@ public class Player {
 		return new Account();
 	}
 
-    /**
-     *
-     * @param j
-     */
-    public void printSlots(int j) {
+	public void printSlots(int j) {
 		System.out.println("Available slots ");
 		for (int i = 0; i < Playground.availablePlaygrounds.get(j).availableHours.size(); i++) {
-			System.out.println( (i + 1) + "- "+Playground.availablePlaygrounds.get(j).availableHours.get(i)); 
+			System.out.println((i + 1) + "- " + Playground.availablePlaygrounds.get(j).availableHours.get(i));
 		}
 	}
 
-    /**
-     *
-     * @return
-     */
-    public int getID() {
+	public int getID() {
 		return id3;
 	}
 }
